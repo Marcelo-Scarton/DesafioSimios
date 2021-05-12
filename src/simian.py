74b01514b705d87f8268cdad11a7e40e5d437dbc
Marcelo Scarton <scartonmarcelo@gmail.com>
2021-05-11 23:05:33 -0300

    Verificação vertical feita

--- src/simian.py
+++ src/simian.py
@@ -4,19 +4,33 @@ sequences = ['AAAA', 'TTTT', 'CCCC', 'GGGG']
 def is_simian(table):
     if len(table[0]) < 4:
         return False
-    if verify_horizontal(table):
+    # horizontal
+    if verify_by_line(table):
+       return True
+    # vertical
+    vertical_table = get_vertical_table(table)
+    if verify_by_line(vertical_table):
         return True
-    else:
-        return False
+    return False
 
 
-def verify_horizontal(table):
+def verify_by_line(table):
     for line in table:
         for sequence in sequences:
             if sequence in line:
+                print(line)
                 return True
     return False
 
 
+def get_vertical_table(table):
+    vertical_table = []
+    aux_table = list(zip(*table))
+    for line in aux_table:
+        value =  "".join(list(line))
+        vertical_table.append(value)
+    return vertical_table
+
+
 if __name__ == '__main__':
-    print(is_simian(['CTGAGA', 'CTGAGC', 'TATTGT', 'AGAGGG', 'CCCCTA', 'TCACTG']))
+    print(is_simian(['CTGAGA', 'CTGAGC', 'TATTGT', 'AGAGGG', 'CCTCTA', 'TCACTG']))
