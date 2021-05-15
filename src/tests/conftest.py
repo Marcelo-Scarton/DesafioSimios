7e904738c17862c8e2585f1b25a0cf7bf5422b9e
Marcelo Scarton <scartonmarcelo@gmail.com>
2021-05-15 19:32:47 -0300

    Melhorando os testes

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -7,11 +7,6 @@ def event():
 
 
 @pytest.fixture(scope="function")
-def invalid_length():
-    return ['AAA', 'TTT', 'CCC']
-
-
-@pytest.fixture(scope="function")
 def invalid_square():
     return ['ATCG', 'AAAT', 'TTTC', 'CGG']
 
@@ -22,8 +17,8 @@ def invalid_table():
 
 
 @pytest.fixture(scope="function")
-def human_4():
-    return ['ATCG', 'AAAT', 'TTTC', 'CGGG']
+def short_table():
+    return ['AAA', 'TTT', 'CCC']
 
 
 @pytest.fixture(scope="function")
@@ -44,3 +39,70 @@ def right_diagonal_simian_4():
 @pytest.fixture(scope="function")
 def left_diagonal_simian_4():
     return ['ATCG', 'AAGG', 'TGAA', 'GCTT']
+
+
+@pytest.fixture(scope="function")
+def human_4():
+    return ['ATCG', 'AAAT', 'TTTC', 'CGGG']
+
+
+@pytest.fixture(scope="function")
+def human_6():
+    return ['ATCGAT', 'TAGGCG', 'TAGCAT', 'AGCCCA', 'AGTCTC', 'AGTTTA']
+
+
+@pytest.fixture(scope="function")
+def human_10():
+    return [
+        'ATCGATCGAT',
+        'CACACACACA',
+        'GTGTGTGTGT',
+        'CACACACACA',
+        'GTGTGTGTGT',
+        'CACACACACA',
+        'GTGTGTGTGT',
+        'CACACACACA',
+        'GTGTGTGTGT',
+        'CACACACACA'
+    ]
+
+
+@pytest.fixture(scope="function")
+def simian_4_A():
+    return ['AACG', 'AACG', 'AAAA', 'CAGA']
+
+
+@pytest.fixture(scope="function")
+def simian_4_T():
+    return ['TTCG', 'ATCG', 'TTTT', 'CTGT']
+
+
+@pytest.fixture(scope="function")
+def simian_4_C():
+    return ['ACCC', 'ACCG', 'CCCC', 'CCGA']
+
+
+@pytest.fixture(scope="function")
+def simian_4_G():
+    return ['AGCG', 'AGGG', 'GGGG', 'GGGA']
+
+
+@pytest.fixture(scope="function")
+def simian_6():
+    return ['ATCGAT', 'TACCCG', 'TAGCAT', 'ACCCCA', 'AGTCTC', 'AGTTTA']
+
+
+@pytest.fixture(scope="function")
+def simian_10():
+    return [
+        'ATCGATCGAT',
+        'ATCGATCGAT',
+        'ATCGATCGAT',
+        'TTTTTTTTTT',
+        'AAAAATAAAA',
+        'CCCCTTCCCC',
+        'GGGTGTTTTT',
+        'TTTTTTTTTT',
+        'AAAAAAAAAA',
+        'CACACACACA'
+    ]
--- src/tests/test_simian.py
+++ src/tests/test_simian.py
@@ -1,51 +1,151 @@
 import json
 
-from src.simian_lambda.lambda_function import lambda_handler, is_simian
+import src.simian_lambda.lambda_function as test_simian
 
 
-def test_lambda_handler_200(event, horizontal_simian_4):
+def test_empty():
+    assert test_simian.is_simian([]) == 400
+
+
+def test_invalid_square(invalid_square):
+    assert test_simian.is_simian(invalid_square) == 400
+
+
+def test_invalid_table(invalid_table):
+    assert test_simian.is_simian(invalid_table) == 400
+
+
+def test_short_table(short_table):
+    assert test_simian.is_simian(short_table) == 403
+
+
+def test_human_4(human_4):
+    assert test_simian.is_simian(human_4) == 403
+
+
+def test_human_6(human_6):
+    assert test_simian.is_simian(human_6) == 403
+
+
+def test_human_10(human_10):
+    assert test_simian.is_simian(human_10) == 403
+
+
+def test_horizontal_simian_4_A(simian_4_A):
+    assert test_simian.verify_by_line(simian_4_A) is True
+
+
+def test_vertical_simian_4_A(simian_4_A):
+    vertical_table = test_simian.get_vertical_table(simian_4_A)
+    assert test_simian.verify_by_line(vertical_table) is True
+
+
+def test_diagonal_simian_4_A(simian_4_A):
+    size = len(simian_4_A)
+    diagonal_table = test_simian.get_diagonal_table(simian_4_A, size, 'r')
+    assert test_simian.verify_by_line(diagonal_table) is True
+
+
+def test_horizontal_simian_4_T(simian_4_T):
+    assert test_simian.verify_by_line(simian_4_T) is True
+
+
+def test_vertical_simian_4_T(simian_4_T):
+    vertical_table = test_simian.get_vertical_table(simian_4_T)
+    assert test_simian.verify_by_line(vertical_table) is True
+
+
+def test_diagonal_simian_4_T(simian_4_T):
+    size = len(simian_4_T)
+    diagonal_table = test_simian.get_diagonal_table(simian_4_T, size, 'r')
+    assert test_simian.verify_by_line(diagonal_table) is True
+
+
+def test_horizontal_simian_4_C(simian_4_C):
+    assert test_simian.verify_by_line(simian_4_C) is True
+
+
+def test_vertical_simian_4_C(simian_4_C):
+    vertical_table = test_simian.get_vertical_table(simian_4_C)
+    assert test_simian.verify_by_line(vertical_table) is True
+
+
+def test_diagonal_simian_4_C(simian_4_C):
+    size = len(simian_4_C)
+    diagonal_table = test_simian.get_diagonal_table(simian_4_C, size, 'l')
+    assert test_simian.verify_by_line(diagonal_table) is True
+
+
+def test_horizontal_simian_4_G(simian_4_G):
+    assert test_simian.verify_by_line(simian_4_G) is True
+
+
+def test_vertical_simian_4_G(simian_4_G):
+    vertical_table = test_simian.get_vertical_table(simian_4_G)
+    assert test_simian.verify_by_line(vertical_table) is True
+
+
+def test_diagonal_simian_4_G(simian_4_G):
+    size = len(simian_4_G)
+    diagonal_table = test_simian.get_diagonal_table(simian_4_G, size, 'l')
+    assert test_simian.verify_by_line(diagonal_table) is True
+
+
+def test_horizontal_simian_6(simian_6):
+    assert test_simian.verify_by_line(simian_6) is True
+
+
+def test_vertical_simian_6(simian_6):
+    vertical_table = test_simian.get_vertical_table(simian_6)
+    assert test_simian.verify_by_line(vertical_table) is True
+
+
+def test_diagonal_simian_6(simian_6):
+    size = len(simian_6)
+    diagonal_table = test_simian.get_diagonal_table(simian_6, size, 'r')
+    assert test_simian.verify_by_line(diagonal_table) is True
+
+
+def test_horizontal_simian_10(simian_10):
+    assert test_simian.verify_by_line(simian_10) is True
+
+
+def test_vertical_simian_10(simian_10):
+    vertical_table = test_simian.get_vertical_table(simian_10)
+    assert test_simian.verify_by_line(vertical_table) is True
+
+
+def test_diagonal_simian_10(simian_10):
+    size = len(simian_10)
+    diagonal_table = test_simian.get_diagonal_table(simian_10, size, 'l')
+    assert test_simian.verify_by_line(diagonal_table) is True
+
+
+def test_lambda_handler_horizontal_200(event, horizontal_simian_4):
     event['body'] = json.dumps({'dna': horizontal_simian_4})
-    response = lambda_handler(event, 'context')
+    response = test_simian.lambda_handler(event, 'context')
+    assert response['statusCode'] == 200
+
+
+def test_lambda_handler_vertical_200(event, vertical_simian_4):
+    event['body'] = json.dumps({'dna': vertical_simian_4})
+    response = test_simian.lambda_handler(event, 'context')
+    assert response['statusCode'] == 200
+
+
+def test_lambda_handler_right_diagonal_200(event, right_diagonal_simian_4):
+    event['body'] = json.dumps({'dna': right_diagonal_simian_4})
+    response = test_simian.lambda_handler(event, 'context')
+    assert response['statusCode'] == 200
+
+
+def test_lambda_handler_left_diagonal_200(event, left_diagonal_simian_4):
+    event['body'] = json.dumps({'dna': left_diagonal_simian_4})
+    response = test_simian.lambda_handler(event, 'context')
     assert response['statusCode'] == 200
 
 
 def test_lambda_handler_403(event, human_4):
     event['body'] = json.dumps({'dna': human_4})
-    response = lambda_handler(event, 'context')
+    response = test_simian.lambda_handler(event, 'context')
     assert response['statusCode'] == 403
-
-
-def test_empty():
-    assert is_simian([]) == 400
-
-
-def test_invalid_length(invalid_length):
-    assert is_simian(invalid_length) == 403
-
-
-def test_invalid_square(invalid_square):
-    assert is_simian(invalid_square) == 400
-
-
-def test_invalid_table(invalid_table):
-    assert is_simian(invalid_table) == 400
-
-
-def test_human_4(human_4):
-    assert is_simian(human_4) == 403
-
-
-def test_horizontal_simian_4(horizontal_simian_4):
-    assert is_simian(horizontal_simian_4) == 200
-
-
-def test_vertical_simian_4(vertical_simian_4):
-    assert is_simian(vertical_simian_4) == 200
-
-
-def test_right_diagonal_simian_4(right_diagonal_simian_4):
-    assert is_simian(right_diagonal_simian_4) == 200
-
-
-def test_left_diagonal_simian_4(left_diagonal_simian_4):
-    assert is_simian(left_diagonal_simian_4) == 200
